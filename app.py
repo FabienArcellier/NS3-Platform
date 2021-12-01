3ddca6b7f849b9cc85da9aa8ee0c55c2fd7dba33
Fabien Arcellier <fabien.arcellier@gmail.com>
2021-12-01 16:01:22 +0100

    configure environment for scalingo

--- app.py
+++ app.py
@@ -338,4 +338,5 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == "__main__" :
-    app.run(debug=True, reloader_interval=20)
+    port = os.getenv('PORT', 80)
+    app.run(host='0.0.0.0', port=port,debug=True, reloader_interval=20)
